7ef88f23977f1d50f84446e64d015b08c2c6393c
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-08-20 00:34:43 -0400

    ready for deploy on adding users

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,7 @@
-// Update with your config settings.
+require('dotenv').config();
+
+const pgConnection = process.env.DATABASE_URL || 'postgresql://postgres@localhost/auth';
+
 
 module.exports = {
 
@@ -19,6 +22,21 @@ module.exports = {
       afterCreate: (conn, done) => {
         conn.run('PRAGMA foreign_keys = ON', done);
       },
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
     },
   },
 
