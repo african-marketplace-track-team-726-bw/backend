64f479b771f657cbaa8c135051ffb7ca9128a721
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-08-20 19:49:59 -0400

    working again

---
+++ database/migrations/20200820184358_products-schema.js
@@ -0,0 +1,31 @@
+exports.up = function(knex) {
+    return knex.schema
+      .createTable("users", tbl => {
+        tbl.increments();
+        tbl.string('email').unique().notNullable();
+        tbl.string('location').notNullable();
+        tbl.string('username').notNullable();
+        tbl.string('name').notNullable();
+        tbl.string('password').notNullable();
+      })
+      .createTable('categorys', tbl => {
+        tbl.increments();
+        tbl.string('category').unique().notNullable();
+      })
+      .createTable('items', tbl => {
+        tbl.increments();
+        tbl.integer('users_id').unsigned().notNullable().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE');
+        tbl.integer('category_id').unsigned().notNullable().references('id').inTable('categorys').onUpdate('CASCADE').onDelete('CASCADE');
+        tbl.string('product').notNullable();
+        tbl.string('description').notNullable();
+        tbl.string('price').notNullable();
+      })
+  }
+  
+  exports.down = function(knex) {
+    return knex.schema
+      .dropTableIfExists('items')
+      .dropTableIfExists('categorys')
+      .dropTableIfExists('users')
+  }; 
+  
