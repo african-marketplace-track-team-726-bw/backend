5de37530321fa47b7793a6f0e95305c3846b530e
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-08-20 20:27:20 -0400

    changed server.js file

--- api/server.js
+++ api/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("helmet");
-// const morgan = process.env.NODE_ENV !== "production" ? require('morgan') : null;
-const morgan = require("morgan");
+const morgan = process.env.NODE_ENV !== "production" ? require('morgan') : null;
+// const morgan = require("morgan");
 
 const authRouter = require('../auth/authRouter.js');
 const usersRouter = require('../users/usersRouter.js');
@@ -11,10 +11,10 @@ const usersRouter = require('../users/usersRouter.js');
 const server = express();
 server.use(helmet());
 server.use(cors());
-// if(morgan) {
-//   server.use(morgan('dev'));
-// }
-server.use(morgan("dev"));
+if(morgan) {
+  server.use(morgan('dev'));
+}
+// server.use(morgan("dev"));
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
