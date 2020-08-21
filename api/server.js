b2778ade8583efb95e500de79f5b2d8cb80644e2
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-08-20 20:37:05 -0400

    server file update

--- api/server.js
+++ api/server.js
@@ -1,21 +1,41 @@
 const express = require("express");
 const helmet = require("helmet");
-const cors = require("helmet");
-const morgan = process.env.NODE_ENV !== "production" ? require('morgan') : null;
-// const morgan = require("morgan");
+const cors = require("cors");
+const session = require('express-session');
+const knexSessionStore = require('connect-session-knex')(session);
 
 const authRouter = require('../auth/authRouter.js');
 const usersRouter = require('../users/usersRouter.js');
 // const itemsRouter = require();
 
 const server = express();
+
+const sessionConfig = {
+  name: 'auth',
+  secret: 'authenticateUser',
+  cookie: {
+      maxAge: 1000 * 60 * 60,
+      secure: false,
+      httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new knexSessionStore(
+      {
+          knex: require('../database/db-config.js'),
+          tablename: 'sessions',
+          sidfieldname: 'sid',
+          createtable: true,
+          clearInterval: 1000 * 60 * 60
+      }
+  )
+};
+
 server.use(helmet());
 server.use(cors());
-if(morgan) {
-  server.use(morgan('dev'));
-}
-// server.use(morgan("dev"));
 server.use(express.json());
+
+server.use(session(sessionConfig));
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
